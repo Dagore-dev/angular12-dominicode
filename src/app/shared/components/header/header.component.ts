88c0f291a191a701498bbe8fb6a26c772f12175a
Dagore <david.gomez.redondo.dev@gmail.com>
2022-09-13 09:46:19 +0200

    extract cart component from header

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { Product } from 'src/app/pages/products/interfaces/product.interface';
-import { ShoppingCartService } from '../../services/shopping-cart.service';
 
 @Component({
   selector: 'app-header',
@@ -9,12 +6,8 @@ import { ShoppingCartService } from '../../services/shopping-cart.service';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-  cart$: Observable<Product[]> = this.shoppingCartService.getCartAction$()
-  quantity$: Observable<number> = this.shoppingCartService.getQuantityAction$() ?? 0
-  total$: Observable<number> = this.shoppingCartService.getTotalAction$() ?? 0
 
-
-  constructor(private shoppingCartService: ShoppingCartService) { }
+  constructor() { }
 
   ngOnInit(): void {
   }
