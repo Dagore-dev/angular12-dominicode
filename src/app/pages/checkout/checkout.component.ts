99b7a7ef3f8c0cb83e0ce7acddfeca34c117b0d4
Dagore <david.gomez.redondo.dev@gmail.com>
2022-09-14 09:05:08 +0200

    stores service

--- src/app/pages/checkout/checkout.component.ts
+++ src/app/pages/checkout/checkout.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { tap } from 'rxjs';
 import { Checkout, DeliveryMode } from './interfaces/checkout.interface';
 import { Store } from './interfaces/store.interface';
+import { StoresService } from './services/stores.service';
 
 @Component({
   selector: 'app-checkout',
@@ -17,9 +19,15 @@ export class CheckoutComponent implements OnInit {
   }
   stores!: Store[]
   
-  constructor() { }
+  constructor(private storesService: StoresService) { }
 
   ngOnInit(): void {
+    this.storesService
+      .getStores()
+      .pipe(
+        tap((response: Store[]) => this.stores = response)
+      )
+      .subscribe()
   }
 
   onSelectedOption(deliveryMode: keyof typeof DeliveryMode): void {
--- src/app/pages/checkout/services/stores.service.ts
+++ src/app/pages/checkout/services/stores.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Store } from '../interfaces/store.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StoresService {
+  private apiURL: string = environment.apiURL + '/stores'
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  getStores(): Observable<Store[]> {
+    return this.httpClient.get<Store[]>(this.apiURL)
+  }
 }
